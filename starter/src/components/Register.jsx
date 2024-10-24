4cfd013d2edf1346196b6dd13dbb67f00e464026
alynch <anthonyvlynch5@gmail.com>
2024-10-24 11:14:05 -0400

    lab3 - edit Login and Register to use useId. Edit Register to show modal with useRef

--- starter/src/components/Register.jsx
+++ starter/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useId, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 import { register as registerOnServer } from '../data/authentication';
@@ -16,62 +16,70 @@ export const Register = ({ setUser }) => {
   const [expiryMonth, setExpiryMonth] = useState("");
   const [expiryYear, setExpiryYear] = useState("");
 
+  const id = useId();
+
+  const dialogRef = useRef(null);
+
   return (
     <>
       <h1>Register</h1>
 
       <div>
-        <label htmlFor={`username`}>Username</label>
-        <input id={`username`} onChange={e => setUsername(e.target.value)} value={username} />
+        <label htmlFor={`username${id}`}>Username</label>
+        <input id={`username${id}`} onChange={e => setUsername(e.target.value)} value={username} />
       </div>
 
       <div>
-        <label htmlFor={`password`}>Password</label>
-        <input id={`password`} onChange={e => setPassword(e.target.value)} value={password} />
+        <label htmlFor={`password${id}`}>Password</label>
+        <input id={`password${id}`} onChange={e => setPassword(e.target.value)} value={password} />
       </div>
 
       <div>
-        <label htmlFor={`first`}>First</label>
-        <input id={`first`} onChange={e => setFirst(e.target.value)} value={first} />
+        <label htmlFor={`first${id}`}>First</label>
+        <input id={`first${id}`} onChange={e => setFirst(e.target.value)} value={first} />
       </div>
 
       <div>
-        <label htmlFor={`last`}>Last</label>
-        <input id={`last`} onChange={e => setLast(e.target.value)} value={last} />
+        <label htmlFor={`last${id}`}>Last</label>
+        <input id={`last${id}`} onChange={e => setLast(e.target.value)} value={last} />
       </div>
 
       <div>
-        <label htmlFor={`email`}>Email</label>
-        <input id={`email`} onChange={e => setEmail(e.target.value)} value={email} />
+        <label htmlFor={`email${id}`}>Email</label>
+        <input id={`email${id}`} onChange={e => setEmail(e.target.value)} value={email} />
       </div>
 
       <div>
-        <label htmlFor={`phone`}>Phone</label>
-        <input id={`phone`} onChange={e => setPhone(e.target.value)} value={phone} />
+        <label htmlFor={`phone${id}`}>Phone</label>
+        <input id={`phone${id}`} onChange={e => setPhone(e.target.value)} value={phone} />
       </div>
 
       <div>
-        <label htmlFor={`imageUrl`}>ImageUrl</label>
-        <input id={`imageUrl`} onChange={e => setImageURL(e.target.value)} value={imageURL} />
+        <label htmlFor={`imageUrl${id}`}>ImageUrl</label>
+        <input id={`imageUrl${id}`} onChange={e => setImageURL(e.target.value)} value={imageURL} />
       </div>
 
       <div>
-        <label htmlFor={`PAN`}>PAN</label>
-        <input id={`PAN`} onChange={e => setPan(e.target.value)} value={pan} />
+        <label htmlFor={`PAN${id}`}>PAN</label>
+        <input id={`PAN${id}`} onChange={e => setPan(e.target.value)} value={pan} />
       </div>
 
       <div>
-        <label htmlFor={`expiryMonth`}>Expiry Month</label>
-        <input id={`expiryMonth`} onChange={e => setExpiryMonth(e.target.value)} value={expiryMonth} />
+        <label htmlFor={`expiryMonth${id}`}>Expiry Month</label>
+        <input id={`expiryMonth${id}`} onChange={e => setExpiryMonth(e.target.value)} value={expiryMonth} />
       </div>
 
       <div>
-        <label htmlFor={`expiryYear`}>Expiry Year</label>
-        <input id={`expiryYear`} onChange={e => setExpiryYear(e.target.value)} value={expiryYear} />
+        <label htmlFor={`expiryYear${id}`}>Expiry Year</label>
+        <input id={`expiryYear${id}`} onChange={e => setExpiryYear(e.target.value)} value={expiryYear} />
       </div>
 
       <button onClick={register}>Register</button>
       <Link to="/login">Login</Link>
+      <dialog ref={dialogRef}>
+        <p>Welcome, {first}!</p>
+        <button onClick={() => dialogRef.current?.close()}>Close</button>
+      </dialog>
     </>
   )
 
@@ -79,7 +87,7 @@ export const Register = ({ setUser }) => {
     const user = { username, password, first, last, phone, email, imageUrl: imageURL, creditCard: { pan, expiryMonth: +expiryMonth, expiryYear: +expiryYear } }
     registerOnServer(user)
       .then(user => setUser(user))
-      .then(() => toast.success(`New user was created`))
+      .then(() => dialogRef.current?.showModal())
       .catch(err => toast.error(`Can't log in. ${err.message}`))
   }
 }
